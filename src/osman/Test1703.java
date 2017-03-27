13eadab60bb0f17cacf0b56a6d1b0af10a47cf1f
odeveci <odeveci@student.tgm.ac.at>
2017-03-27 11:06:42 +0200

    second commit

--- src/osman/Test1703.java
+++ src/osman/Test1703.java
@@ -4,5 +4,7 @@ public class Test1703 {
 
 	public static void gutenTag() {
 		System.out.println("Hallo Osman!");
+		System.out.println("Hallo Osman!");
+		System.out.println("Hallo Osman!");
 	}
 }
